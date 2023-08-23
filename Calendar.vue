ca9d0cc3d1bf0511e41005770aedb9d5c609b4e3
Jtrom314 <97546727+Jtrom314@users.noreply.github.com>
2023-08-23 09:00:45 -0600

    Create Calendar.vue

---
+++ Calendar.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="calendar-width bg-base rounded-md px-4 pt-2 pb-4" v-if="calendarLoaded" ref="calendar">
+    <div class="grid grid-cols-7">
+        <div class="flex justify-center col-span-7">
+          <p class="text-lg font-bold text-primary px-4">{{currentMonth.name}}</p>
+        </div>
+    <p v-for="dayName in dayNames" :key="dayName" class="flex justify-center font-bold text-primary">{{dayName}}</p>
+    <AttendanceCalendarDate
+      v-for="day in monthInfo[currentMonth.year][currentMonth.name]"
+      :day="day"
+
+    />
+  </div>
+ </div>
+</template>
